09bc164dcd96f106921bffef70a37526ec070b2d
mpapallo <michaelapapallo@gmail.com>
2015-02-12 12:18:25 -0500

    animation def works

--- KnightsTour/Driver.java
+++ KnightsTour/Driver.java
@@ -1,7 +1,6 @@
 public class Driver{
     public static void main(String[] args){
-
-	KnightsTour tour = new KnightsTour(6);
-        System.out.println(tour);
+	KnightsTour tour = new KnightsTour(5);
+	tour.solve();
     }
 }
--- KnightsTour/KnightsTour.java
+++ KnightsTour/KnightsTour.java
@@ -23,73 +23,65 @@ public class KnightsTour{
 	}
     }
 
+    public String name(){
+	return "Papallo,Michaela";
+    }
+
     public String toString(){
 	String ans = "\n";
-	//build your knights tour here...
-	for (int i = 0; i < board[0].length; i ++){
-	    ans += " __ ";
-	}
-	ans += "\n";
 	for (int y = 0; y < board.length; y ++){
-	    ans += "|";
 	    for (int x = 0; x < board[0].length; x ++){
 		if (board[y][x] < 10){
-		    ans += " " + board[y][x] + "|" ;
+		    ans += "  " + board[y][x];
 		}else{
-		    ans += board[y][x] + "|";
+		    ans += " " + board[y][x];
 		}
 	    }
 	    ans += "\n";
-	    for (int i = 0; i < board[0].length; i ++){
-		ans += " __ ";
-	    }
-	    ans += "\n";
 	}
-	return clear +hide + go(0,0) + ans + "\n" + show;
+	return hide + clear + go(0,0) + ans + "\n" + show;
     }
 
     public KnightsTour(int size){
 	board = new int[size][size];
 	for (int y = 0; y < size; y ++){
-	    for (int x = 0; x  < size; x ++){
-		board[y][x] = -1;
+	    for (int x = 0; x < size; x ++){
+		board[y][x] = 0;
 	    }
 	}
     }
 
-    public void solve(){
-	solve(0, 0);
+    public boolean solve(){
+	return solve(0, 0);
     }
 
-    public void solve(int startx, int starty){
-	solve(startx, starty, 0);
+    public boolean solve(int startx, int starty){
+	return solve(startx, starty, 0);
     }
 		
     public boolean solve(int x,int y,int currentMoveNumber){
-	//System.out.println(this);
-	//wait(20);
-	if (y >= board.length || x >= board[0].length){
+	System.out.println(this);
+	wait(30);
+	if (y < 0 || x < 0 || y >= board.length || x >= board[0].length){
 	    return false;
 	}
-	try{
-	    if (board[y][x] == 0 && board.length * board.length == currentMoveNumber - 1){
+	if (board[y][x] == 0 && board.length * board.length == currentMoveNumber - 1){
+	    return true;
+	}
+	if (board[y][x] == 0){
+	    board[y][x] = currentMoveNumber;
+	    if (solve(x + 2, y + 1, currentMoveNumber + 1) ||
+		solve(x - 2, y + 1, currentMoveNumber + 1) ||
+		solve(x + 2, y - 1, currentMoveNumber + 1) ||
+		solve(x - 2, y - 1, currentMoveNumber + 1) ||
+		solve(x + 1, y + 2, currentMoveNumber + 1) ||
+		solve(x - 1, y + 2, currentMoveNumber + 1) ||
+		solve(x + 1, y - 2, currentMoveNumber + 1) ||
+		solve(x - 1, y - 2, currentMoveNumber + 1)){
 		return true;
 	    }
-	    if (board[y][x] == -1){
-		board[y][x] = currentMoveNumber;
-		if (solve(x + 2, y + 1, currentMoveNumber + 1) ||
-		    solve(x - 2, y + 1, currentMoveNumber + 1) ||
-		    solve(x + 2, y - 1, currentMoveNumber + 1) ||
-		    solve(x - 2, y - 1, currentMoveNumber + 1) ||
-		    solve(x + 1, y + 2, currentMoveNumber + 1) ||
-		    solve(x - 1, y + 2, currentMoveNumber + 1) ||
-		    solve(x + 1, y - 2, currentMoveNumber + 1) ||
-		    solve(x - 1, y - 2, currentMoveNumber + 1)){
-		    return true;
-		}
-		board[y][x] = -1;
-	    }
-	}catch(Exception e){}
+	    board[y][x] = 0;
+	}
 	return false;
     }
 }
