993f283e7f66dcf39676913180ae8205ec3957d0
mpapallo <michaelapapallo@gmail.com>
2015-02-23 12:21:01 -0500

    starting lake problem

---
+++ MakeLake.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class MakeLake{
+
+    private File input;
+    private Scanner scan;
+    private String[][] pasture;
+    private int e;
+    private int n;
+    
+    public MakeLake(){
+	
+	try{
+	    input = new File("makelake.txt");
+	    scan = new Scanner(input);
+	}catch(Exception e){}
+	pasture = new String[Integer.parseInt(scan.next())][Integer.parseInt(scan.next())];
+	e = Integer.parseInt(scan.next());
+	n = Integer.parseInt(scan.next());
+	
+	for (int r = 0; r < pasture.length; r ++){
+	    for (int c = 0; c < pasture[0].length; c ++){
+		pasture[r][c] = scan.next();
+	    }
+	}
+      
+    }
+
+
+}
